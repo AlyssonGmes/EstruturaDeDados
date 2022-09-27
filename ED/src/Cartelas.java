60eaa0b709fb01ddbfab91eaa02d90829dc0d5f5
Alysson <54059977+AlyssonGmes@users.noreply.github.com>
2022-09-27 18:47:02 -0300

    Update Cartelas.java

--- ED/src/Cartelas.java
+++ ED/src/Cartelas.java
@@ -1,26 +1,36 @@
 import java.util.Random;
-import java.util.Set;
 
 public class Cartelas {
-    private final int[][] casas = new int[5][5];
     private final Random rnd = new Random();
 
     //gera 1 cartela
     private int[][] gerarCartela() {
+        final int[][] casas = new int[5][5];
 
         for (int i = 0; i < 5; i++) {
             for (int k = 0; k < 5; k++) {
-                casas[i][k] = rnd.nextInt(59)+1;
+                casas[i][k] = rnd.nextInt(59) + 1;
             }
 
-            if(i==4){
-                if(validarCartela(casas)){
-                  //     i = 0;
+            if(i == 4){
+                if(validarTabela(casas)){
+                    i = 0;
                 }
             }
         }
 
         return casas;
+    }
+
+    //retorna um array de objetos contendo n cartelas
+    public Object[] gerarBingo(int numDeCartelas) {
+        Object[] arrCartelas = new Object[numDeCartelas];
+
+        for (int i = 0; i < numDeCartelas; i++) {
+            arrCartelas[i] = gerarCartela();
+
+        }
+        return arrCartelas;
     }
 
     //exibe todas as cartelas a partir de um array de objetos
@@ -29,52 +39,47 @@ public class Cartelas {
 
         for (Object n : arr) {
             //pegar cartelas
-                num = (int[][]) n;
-                //imprimir cartela
-                for (int j = 0; j < 5; j++) {
-                    for (int k = 0; k < 5; k++) {
-                        if (num[j][k] < 10) {
-                            System.out.print("0" + num[j][k] + " ");
-                        } else {
-                            System.out.print(num[j][k] + " ");
-                        }
+            num = (int[][]) n;
+            //imprimir cartela
+            for (int j = 0; j < 5; j++) {
+                for (int k = 0; k < 5; k++) {
+                    if (num[j][k] < 10) {
+                        System.out.print("0" + num[j][k] + " ");
+                    } else {
+                        System.out.print(num[j][k] + " ");
                     }
-                    System.out.println();
                 }
-                System.out.println("\n");
+                System.out.println();
+            }
+            System.out.println("\n");
         }
     }
 
 
-    //retorna um array de objetos contendo n tabelas
-    public Object[] gerarBingo(int numDeCartelas) {
-        Object[] arrCartelas = new Object[numDeCartelas];
+    //retorna true se a cartela possuir números iguais
+    public boolean validarTabela(int[][] arrCartela) {
+        //ponteiro 1
+        for (int i = 0; i < 5; i++) {
+            for (int k = 0; k < 5; k++) {
 
-        for (int i = 0; i < numDeCartelas; i++) {
-            arrCartelas[i] = gerarCartela();
-        }
+                //ponteiro 2
+                for (int i1 = 0; i1 < 5; i1++) {
+                    for (int k1 = 0; k1 < 5; k1++) {
 
-        return arrCartelas;
-    }
-
-    //retorna true se algum elemento for igual
-    private boolean validarCartela(int[][] cartela) {
-        for (int linha = 0; linha < 5; linha++) {
-            for (int col = 0; col < 5; col++) {
-
-                for (int l = 0; l < 5; l++) {
-                    for (int c = 0; c < 5; c++) {
-
-                        if(linha != l && col != c){
-                            if(cartela[linha][col] == cartela[l][c]){
-                                System.out.println(cartela[linha][col]+ " repetido\n");
+                        //para não apontar para a mesma posição
+                        if(i != i1 || k !=k1){
+                            if(arrCartela[i][k] == arrCartela[i1][k1]){
+                                System.out.println(arrCartela[i][k]);
                                 return true;
                             }
                         }
                     }
                 }
+
             }
         }
+
         return false;
     }
+
 }
