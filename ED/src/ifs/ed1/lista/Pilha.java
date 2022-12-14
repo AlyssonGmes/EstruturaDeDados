60a7c7f01a7fa3e12deb8f2a59b5809fb35f46fb
Alysson <54059977+AlyssonGmes@users.noreply.github.com>
2022-12-14 17:10:01 -0300

    Update Pilha.java
    Pilha.

--- ED/src/ifs/ed1/lista/Pilha.java
+++ ED/src/ifs/ed1/lista/Pilha.java
@@ -1,48 +1,98 @@
 package ifs.ed1.lista;
 
-public class Pilha extends ListaEnc<Integer>{
-    @Override
-    public void incluir(Integer elemento) throws Exception {
+public class Pilha<T extends Comparable<T>>   {
 
-    }
+    No<T> noInicioLista;
+    No<T> noFimLista;
+    int contadorNos = 0;
 
-    @Override
-    public void incluirInicio(Integer elemento) throws Exception {
+    public void incluir(T elemento) throws Exception {
+        incluirInicio(elemento);
+    } //ok
 
-    }
+    public void incluirInicio(T elemento) throws Exception {
+        if (noInicioLista.getDado() == null) {
+            noInicioLista.setDado(elemento);
+            noInicioLista.setProximo(noFimLista);
+            contadorNos++;
+        } else {
+            No<T> novoInicio = new No<>();
+            novoInicio.setDado(elemento);
+            novoInicio.setProximo(noInicioLista);
+            noInicioLista = novoInicio;
+            contadorNos++;
+        }
+    } //ok
 
-    @Override
-    public void incluir(Integer elemento, int posicao) throws Exception {
+    public T get(int posicao) throws Exception {
+        if (posicao > contadorNos - 1 || posicao < 0) {
+            throw new Exception("Posição solicitada não existe na lista");
+        } else {
+            No<T> auxiliar = noInicioLista;
 
-    }
+            for (int i = 0; i < posicao; i++) {
+                auxiliar = auxiliar.getProximo();
+            }
+            return auxiliar.getDado();
+        }
+    } //ok
 
-    @Override
-    public Integer get(int posicao) throws Exception {
-        return null;
-    }
+    public int getPosElemento(T elemento) throws Exception {
+        No<T> auxiliar = noInicioLista;
+        for (int i = 0; i < contadorNos; i++) {
+            if (auxiliar.getDado().equals(elemento)) {
+                return i;
+            }
+            auxiliar = auxiliar.getProximo();
+        }
 
-    @Override
-    public int getPosElemento(Integer elemento) throws Exception {
-        return 0;
-    }
+        return -1;
+    } //ok
 
-    @Override
-    public void remover(int posicao) throws Exception {
+    public void shift() throws Exception {
+        if (contadorNos == 0) {
+            throw new Exception("A lista está vazia");
+        } else if (contadorNos == 1) {
+            limpar();
+        } else {
+            noInicioLista = noInicioLista.getProximo();
+            contadorNos--;
+        }
+    } //ok
 
-    }
-
-    @Override
     public void limpar() {
-
+        noInicioLista = new No<>();
+        noFimLista = new No<>();
+        contadorNos = 0;
     }
 
-    @Override
     public int getTamanho() {
-        return 0;
+        return contadorNos;
+    } //ok
+
+    public boolean contem(T elemento) throws Exception {
+        No<T> auxiliar = noInicioLista;
+
+        while (auxiliar.getProximo() != null) {
+            if (auxiliar.dado.equals(elemento)) {
+                return true;
+            }
+            auxiliar = auxiliar.getProximo();
+        }
+
+        return false;
     }
 
-    @Override
-    public boolean contem(Integer elemento) throws Exception {
-        return false;
+    public void listar() {
+        No<T> auxiliar = noInicioLista;
+        while (auxiliar.getProximo() != null && auxiliar.getProximo().dado != null) {
+            System.out.print(auxiliar.dado + " ");
+            auxiliar = auxiliar.getProximo();
+        }
+        if (noInicioLista.dado == null) {
+            System.out.println("A lista está vazia.");
+        } else {
+            System.out.println(auxiliar.dado);
+        } //ok
     }
 }
